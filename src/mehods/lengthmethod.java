4b84503d6204554488022cb39744d7db45343bb4
VishwanathShidramayyanamath <vishu.smath@gmail.com>
2021-10-28 23:34:05 +0530

    assingnment

---
+++ src/mehods/lengthmethod.java
@@ -0,0 +1,9 @@
+package mehods;
+
+public class lengthmethod {
+public static void main(String[] args) {
+	int[] ide =dida (23,34,55);
+	System.out.println(int.length);
+	System.out.println(int.length());
+}
+}
